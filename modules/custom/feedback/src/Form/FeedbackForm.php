de55e7e7624546bec2094579cb77d5fa091ac430
meylisday <victory.sett@gmail.com>
2019-06-14 15:14:04 +0300

    Added integration with hubspot

--- modules/custom/feedback/src/Form/FeedbackForm.php
+++ modules/custom/feedback/src/Form/FeedbackForm.php
@@ -71,7 +71,6 @@ class FeedbackForm extends FormBase {
         }
 
     }
-
     /**
      * {@inheritdoc}
      */
@@ -85,10 +84,50 @@ class FeedbackForm extends FormBase {
         if ($result['result'] == true) {
             $this->messenger()->addMessage($this->t('Your message has been sent.'));
             Drupal::logger('feedback')->notice('E-Mail has been send to: '. $from);
+            $response = $this->createContact($form_state);
+            if($response) {
+                $this->messenger->addMessage($this->t('Contact created.'));
+            }
         }
         else {
             $this->messenger()->addMessage($this->t('There was a problem sending your message and it was not sent.'), 'error');
         }
+    }
 
+    public function createContact(FormStateInterface $form_state){
+        $form_values = $form_state->getValues();
+        $arr = array(
+            'properties' => array(
+                array(
+                    'property' => 'email',
+                    'value' => $form_values['email']
+                ),
+                array(
+                    'property' => 'firstname',
+                    'value' => $form_values['first_name']
+                ),
+                array(
+                    'property' => 'lastname',
+                    'value' => $form_values['last_name']
+                )
+            )
+        );
+        $post_json = json_encode($arr);
+        $hapikey = 'ab0b93e5-778f-4085-914c-98bd3c0e9b7a';
+        $endpoint = 'https://api.hubapi.com/contacts/v1/contact?hapikey=' . $hapikey;
+        $ch = @curl_init();
+        @curl_setopt($ch, CURLOPT_POST, true);
+        @curl_setopt($ch, CURLOPT_POSTFIELDS, $post_json);
+        @curl_setopt($ch, CURLOPT_URL, $endpoint);
+        @curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        @curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = @curl_exec($ch);
+        $status_code = @curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $curl_errors = curl_error($ch);
+        @curl_close($ch);
+        echo "curl Errors: " . $curl_errors;
+        echo "\nStatus code: " . $status_code;
+        echo "\nResponse: " . $response;
+        return $response;
     }
 }
